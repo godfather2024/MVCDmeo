1f6d520d8841b49c91ed967517f4e0e9a95e1e5d
godfather2024 <dsura001@ucr.edu>
2017-02-21 21:32:53 -0700

    Add project files.

---
+++ MVCDmeo/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDmeo.Models;
+
+namespace MVCDmeo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeContext empcontext = new EmployeeContext();
+            List<Employee> emp = empcontext.Employees.ToList();
+
+            return View(emp);
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            EmployeeContext empcontext = new EmployeeContext();
+            //List<Employee> emp = empcontext.Employees.ToList();
+            Employee emp = empcontext.Employees.Single(x => x.EmpId == id);
+            return View(emp);
+        }
+    }
+}
---
+++ MVCDmeo/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCDmeo.Models
+{
+    [Table("[EmployeeDetail]")]
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+
+    }
+}
